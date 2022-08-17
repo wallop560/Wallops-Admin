741c0a89f3092bbee7af107e56d4aabee1b65e1d
wallop560 <72463875+wallop560@users.noreply.github.com>
2022-08-17 23:52:08 +0100

    Update Commands.lua

--- Commands.lua
+++ Commands.lua
@@ -1,1 +1,70 @@
+local Cmds = {}
 
+local FakeCmds = {}
+setmetatable(FakeCmds,{
+	__newIndex = function(table,index,value)
+		return
+	end,
+	__call = function()
+		return 
+	end
+})
+function FakeCmds:AddCommand(CommandNames,Arguments,Description,Call)
+	assert(type(CommandNames) == 'table' or type(CommandNames) == 'string','Argument 1 must be a string or a table got ' .. type(CommandNames))
+	assert(type(Arguments) == 'table','Argument 2 must be a table got ' .. type(Arguments))
+	assert(type(Description) == 'string','Argument 3 must be a table got ' .. type(Description))
+	assert(type(Call) == 'function','Argument 4 must be a table got ' .. type(Call))
+
+	if type(CommandNames) == 'string' then
+		CommandNames = {CommandNames}
+	end
+	for i,v in next,CommandNames do
+		CommandNames[i] = string.lower(v)
+	end
+	table.insert(Cmds,setmetatable({
+		CommandNames = CommandNames,
+		Arguments = Arguments,
+		Description = Description,
+		Call = Call
+	},{
+		__call = function(table)
+			table.call()
+		end
+	}))
+end
+
+function FakeCmds:GetCommands()
+	return Cmds
+end
+
+function FakeCmds:GetCommand(Name)
+    for _,Command in next,Cmds do -- check for exact match
+		for _,CommandName in next,Command.CommandNames do
+			if CommandName == string.lower(Text) then
+				return Text,Command,true
+			end
+		end
+	end
+    error('Command not found')
+end
+
+function FakeCmds:GetClosestCommand(Text)
+	for _,Command in next,Cmds do -- check for exact match
+		for _,CommandName in next,Command.CommandNames do
+			if CommandName == string.lower(Text) then
+				return Text,Command,true
+			end
+		end
+	end
+	for _,Command in next,Cmds do -- check for Closest match
+		for _,CommandName in next,Command.CommandNames do
+			if string.sub(CommandName,1,#Text) == string.lower(Text) then
+				local OtherText = string.sub(CommandName,#Text+1,#CommandName)
+				return Text..OtherText,Command,false
+			end
+		end
+	end
+	return Text,nil,false -- returns text, no command and not exact match
+end
+
+return FakeCmds
