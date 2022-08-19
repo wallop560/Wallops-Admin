0ea848c0fdb5b1b047bdd1031c80540e2ed1ac58
wallop560 <72463875+wallop560@users.noreply.github.com>
2022-08-19 23:46:51 +0100

    Update CommandList.lua

--- CommandList.lua
+++ CommandList.lua
@@ -1,21 +1,23 @@
-local Util = loadstring
-return {
-    -- All games
+local Util = loadstring(game:HttpGet('https://raw.githubusercontent.com/wallop560/Wallops-Admin/main/Util.lua'))()
+local Commands = {}
 
-    All = {
-        {
-            Names = 'kill',
-            Arguments = {'Player'},
-            Description = 'Kills the specified Player.',
-            Call = function(Player)
-
-            end
-        },
-        {
-            Names = {'view','spectate'},
-            Arguments = {'Player'},
-            Description = 'Makes your camera follow a specific player'
-             
-        }
+Commands.Global = {
+    {
+        Names = 'kill',
+        Arguments = {'Player'},
+        Description = 'Kills the specified Player.',
+        Call = function(Player)
+            print('kill',Player)
+        end
+    },
+    {
+        Names = {'view','spectate'},
+        Arguments = {'Player'},
+        Description = 'Makes your camera follow a specific player',
+        Call = function(Player)
+            print('view',Player)
+        end
     }
 }
+
+return Commands
