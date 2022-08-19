b969cbc78b3921efd24217399d00ffcc0b99fcd2
wallop560 <72463875+wallop560@users.noreply.github.com>
2022-08-19 23:38:48 +0100

    Update Ui.lua

--- Ui.lua
+++ Ui.lua
@@ -75,6 +75,28 @@ local Admin_UI = Create(
 								Size = UDim2.new(1, 0, 1, 0),
 								TextColor3 = Color3.new(0.843137, 0.839216, 0.85098),
 								Text = '',
+								ZIndex = 4,
+								Font = Enum.Font.SourceSansBold,
+								BackgroundTransparency = 1,
+								TextXAlignment = Enum.TextXAlignment.Left,
+								FontSize = Enum.FontSize.Size18,
+								BackgroundColor3 = Color3.new(1, 1, 1)
+							},{
+								Create(
+									'UIPadding',{
+										PaddingLeft = UDim.new(0, 10)
+									}
+								)
+							}
+						),
+						Create(
+							'TextLabel',{
+                                Name = 'CmdPreview',
+								LineHeight = 1.100000023841858,
+								TextColor3 = Color3.new(0.427451, 0.443137, 0.478431),
+								TextSize = 16,
+								Size = UDim2.new(1, 0, 1, 0),
+								Text = '',
 								ZIndex = 3,
 								Font = Enum.Font.SourceSansBold,
 								BackgroundTransparency = 1,
@@ -95,25 +117,54 @@ local Admin_UI = Create(
 		)
 	}
 )
-Admin_UI.Parent = game.CoreGui
+
+-- This part was ripped from IY cuz i only have SynapseX
+local COREGUI = game:GetService('CoreGui')
+function randomString()
+	local length = math.random(10,20)
+	local array = {}
+	for i = 1, length do
+		array[i] = string.char(math.random(32, 126))
+	end
+	return table.concat(array)
+end
+if get_hidden_gui or gethui then
+	local hiddenUI = get_hidden_gui or gethui
+	Admin_UI.Name = randomString()
+	Admin_UI.Parent = hiddenUI()
+elseif (not is_sirhurt_closure) and (syn and syn.protect_gui) then
+	Admin_UI.Name = randomString()
+	syn.protect_gui(Admin_UI)
+	Admin_UI.Parent = COREGUI
+	PARENT = Main
+elseif COREGUI:FindFirstChild('RobloxGui') then
+	PARENT = COREGUI.RobloxGui
+else
+	local Main = Instance.new("ScreenGui")
+	Main.Name = randomString()
+	Main.Parent = COREGUI
+	PARENT = Main
+end
 
 local OrigonalSize = Admin_UI.CmdBar.Size
 
 Admin_UI.CmdBar.TextFrame.CmdBox:GetPropertyChangedSignal('TextBounds'):Connect(function()
     local textBounds = Admin_UI.CmdBar.TextFrame.CmdBox.TextBounds.X + 20
     local Bounds = Admin_UI.CmdBar.TextFrame.AbsoluteSize.X 
+	local OtherTextBounds = Admin_UI.CmdBar.TextFrame.CmdPreview.TextBounds.X + 20
 
+	local TextBound = textBounds > Bounds and textBounds or OtherTextBounds
 
-    if textBounds > Bounds then
+    if TextBound > Bounds  then
         print(textBounds,OrigonalSize.X.Offset)
-        game:GetService('TweenService'):Create(Admin_UI.CmdBar,TweenInfo.new(.1,Enum.EasingStyle.Quad),{Size = UDim2.new(0,textBounds+10,0,OrigonalSize.Y.Offset)}):Play()
+        game:GetService('TweenService'):Create(Admin_UI.CmdBar,TweenInfo.new(.1,Enum.EasingStyle.Quad),{Size = UDim2.new(0,TextBound+10,0,OrigonalSize.Y.Offset)}):Play()
     else
         print(textBounds,OrigonalSize.X.Offset)
         game:GetService('TweenService'):Create(Admin_UI.CmdBar,TweenInfo.new(.1,Enum.EasingStyle.Quad),{Size = OrigonalSize}):Play()
     end
 end)
 
-local UI = {ScreenGui=Admin_UI,Main=Admin_UI.CmdBar,TextBox=Admin_UI.CmdBar.TextFrame.CmdBox,DropShadow = Admin_UI.CmdBar.DropShadow}
+local UI = {ScreenGui=Admin_UI,Main=Admin_UI.CmdBar,TextBox=Admin_UI.CmdBar.TextFrame.CmdBox,Preview = Admin_UI.CmdBar.TextFrame.CmdPreview,DropShadow = Admin_UI.CmdBar.DropShadow}
 
 function UI:ApplyTheming(Instance)
     if Instance:IsA('TextLabel') or Instance:IsA('TextButton') then 
@@ -144,8 +195,6 @@ function UI:NewWindow(Size,Position)
 
     return MainFrame
 end
-
-UI.Create = Create
 
 
 return UI
