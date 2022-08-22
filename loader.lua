0369ec9d44a8481f319ecf34a614fdf43915c78b
wallop560 <72463875+wallop560@users.noreply.github.com>
2022-08-22 15:29:50 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
-((syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)) = game:HttpGet('https://raw.githubusercontent.com/wallop560/Wallops-Admin/main/loader.lua')
+((syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport))(game:HttpGet('https://raw.githubusercontent.com/wallop560/Wallops-Admin/main/loader.lua'))
 loadstring(game:HttpGet('https://raw.githubusercontent.com/wallop560/Wallops-Admin/main/main.lua'))()
